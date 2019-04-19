8b75c159fabeb61438c33e6c1b15176b11dab619
NgocTo <baongoc1308@gmail.com>
2019-04-19 01:09:47 -0400

    Updates

---
+++ precache-manifest.01603c12ba3384a966ef609ffb52c241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "578ec561a56236423073",
+    "url": "/colourlover/static/js/runtime~main.2f8e8a18.js"
+  },
+  {
+    "revision": "28b38dd120b89969c521",
+    "url": "/colourlover/static/js/main.aed4bea7.chunk.js"
+  },
+  {
+    "revision": "253c56a3e8d739916d96",
+    "url": "/colourlover/static/js/2.13ebb4f1.chunk.js"
+  },
+  {
+    "revision": "28b38dd120b89969c521",
+    "url": "/colourlover/static/css/main.fc72cc61.chunk.css"
+  },
+  {
+    "revision": "66c5285ac3af0e9e6e0aed4de740608e",
+    "url": "/colourlover/index.html"
+  }
+];
